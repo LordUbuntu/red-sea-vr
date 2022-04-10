378bcaebb21672e2ddc80dd34954912036658673
Jacobus Burger <therealjacoburger@gmail.com>
2022-04-09 17:25:01 -0700

    Finalized MovementSoundProvider script
    player movement will now produce sound. It may still produce walking sounds when players fall and will play the sound effect fully. In all it's good.

--- Moses Project/Assets/Scripts/MovementSoundProvider.cs
+++ Moses Project/Assets/Scripts/MovementSoundProvider.cs
@@ -5,33 +5,20 @@ using UnityEngine;
 public class MovementSoundProvider : MonoBehaviour
 {
     public AudioSource walkingSFX;
-
     private CharacterController player;
-    private float waitTime;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<CharacterController>();
-        waitTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // play sound if not playing and player moving
         if ( walkingSFX.isPlaying == false )
-        {
-            Debug.Log("velocity magnitude: " + player.velocity.magnitude);
             if ( player.velocity.magnitude >= 0.2 )
-            {
-                Debug.Log("playing sound");
                 walkingSFX.Play();
-            }
-            else
-            {
-                Debug.Log("stopping sound");
-                walkingSFX.Stop();
-            }
-        }
     }
 }
